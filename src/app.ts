8d9be74c4d4cce44f069b2b19afd4139fc040806
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-22 00:31:48 -0400

    arrow functions and lexical scope

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,37 @@
 
 //Object literal
 
-const myObj = {
+// const myObj = {
+//     myMethod() {
+//         console.log(this)
+//     }
+// }
+
+// function myFunction(...text: string[]) {
+//     console.log(this, ...text)
+// }
+
+// myFunction.call('myObj')
+// myFunction.call(myObj, 'ABC', 'DEF')
+// myFunction.apply(myObj, ['ABC', 'DEF'])
+
+// const bindFunction = myFunction.bind(myObj);
+// bindFunction('ABC', 'DEF', 'GHI');
+// bindFunction('CEF', 'DDEFEF', 'WED');
+
+class MyClass {
     myMethod() {
-        console.log(this)
+        const foo = 123;
+        console.log('1', this, foo)
+        setTimeout(() => {
+            console.log('2', this, foo);
+        });
+        setTimeout(function () {
+            console.log('3', this, foo);
+        });
     }
 }
 
-function myFunction(...text: string[]) {
-    console.log(this, ...text)
-}
+const myInstance = new MyClass();
 
-myFunction.call('myObj')
-myFunction.call(myObj, 'ABC', 'DEF')
-myFunction.apply(myObj, ['ABC', 'DEF'])
-
-const bindFunction = myFunction.bind(myObj);
-bindFunction('ABC', 'DEF', 'GHI');
-bindFunction('CEF', 'DDEFEF', 'WED');
+myInstance.myMethod();
